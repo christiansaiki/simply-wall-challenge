7ce4e5c610a0559d9a974daa82a20294ceba7f54
Christian Saiki <christian_saiki@hotmail.com>
2024-04-02 17:17:40 +1100

    Added more initial NestJS setup

---
+++ backend/src/app.module.ts
@@ -0,0 +1,40 @@
+import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CompanyModule } from './company/company.module';
+import { CompanyScoreModule } from './companyScore/companyScore.module';
+import { CompanyPriceCloseModule } from './companyPriceClose/companyPriceClose.module';
+
+// Otherwise e2e fails with EntityMetadataNotFoundError: No metadata for "CompanyModel" was found
+const entities =
+  process.env.NODE_ENV === 'test'
+    ? ['src/**/*.model{.ts,.js}']
+    : ['dist/**/*.model{.ts,.js}'];
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: __dirname + '/../db/sws.sqlite3',
+      entities,
+      synchronize: false,
+      logging: true,
+      namingStrategy: new SnakeNamingStrategy(),
+    }),
+    CompanyModule,
+    CompanyScoreModule,
+    CompanyPriceCloseModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
